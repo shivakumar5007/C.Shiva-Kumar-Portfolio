0bce08c1e04e4bd992f67f119bbe0c15171cd6a1
shivakumar5007 <changomula.shivakumar@gmail.com>
2024-01-30 20:05:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu()
+{
+    const menu=document.querySelector(".mymenu-links");
+    const icon=document.querySelector(".mymenu-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
